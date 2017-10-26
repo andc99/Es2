da0412747b66ae8d2dbfc76e9bd8d60528e11c82
andc99 <32933932+andc99@users.noreply.github.com>
2017-10-26 14:57:54 +0200

    Create es2.sh

---
+++ es2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+N=$1
+M=$2
+
+if [ $# -eq 2 ] && [$M < 32767]; then
+  echo "Inserire almeno due numeri, N e m" 1>&2
+else
+  for (( i=0; i<$N; i++ )); do
+    echo $(($RANDOM % $M))
+fi
